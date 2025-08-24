b2586a8b0a0843fb219a24f0b117cfb43fd2fba4
Archit Anirudhan <architanirudhan123@gmail.com>
2025-08-24 21:07:31 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+
+const router = createBrowserRouter([
+  {
+    element,
+  },
+]);
+
+function App() {
+  return <RouterProvider router={router} />;
+}
+
+export default App;
